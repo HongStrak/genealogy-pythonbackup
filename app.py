af71a1975554ad89c34e2a3f40b4561e0f034464
jayce <2434128704@qq.com>
2022-09-09 18:24:01 +0800

    sync

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
-from fastapi import FastAPI
-import uvicorn
+# from fastapi import FastAPI
+# import uvicorn
 
-app = FastAPI()
-
-
-@app.get('/hello')
-def hello():
-    return 'hello, fastapi'
+# app = FastAPI()
 
 
-if __name__ == '__main__':
-    uvicorn.run(app, host='0.0.0.0', port=8199)
+# @app.get('/hello')
+# def hello():
+#     return 'hello, fastapi'
+
+
+# if __name__ == '__main__':
+#     uvicorn.run(app, host='0.0.0.0', port=8199)
  
