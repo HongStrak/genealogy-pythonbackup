4669f8f108c03a210cffee90fd308bdea9980c1a
jayce <2434128704@qq.com>
2022-09-09 18:24:25 +0800

    sync

--- app.py
+++ app.py
@@ -11,4 +11,15 @@
 
 # if __name__ == '__main__':
 #     uvicorn.run(app, host='0.0.0.0', port=8199)
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'hello world'
+    
+if __name__ == '__main__':
+    app.run()
  
